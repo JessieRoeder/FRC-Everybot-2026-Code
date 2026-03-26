5fb731a47178c6ce1b1a1943ea4310347b1b3c40
Jessie Roeder <36315040+JessieRoeder@users.noreply.github.com>
2026-03-26 08:44:38 -0400

    edited constants to make INDEXER_INTAKING_PERCENT default positive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -43,7 +43,7 @@ public final class Constants {
     public static final int LAUNCHER_MOTOR_CURRENT_LIMIT = 80;
 
     // All values likely need to be tuned based on your robot
-    public static final double INDEXER_INTAKING_PERCENT = -.8; 
+    public static final double INDEXER_INTAKING_PERCENT = .8; 
     public static final double INDEXER_LAUNCHING_PERCENT = 0.6;
     public static final double INDEXER_SPIN_UP_PRE_LAUNCH_PERCENT = -0.5;
 
